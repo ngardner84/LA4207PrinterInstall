6b7c8e02fd78a818d4a692d6907b33916f32fbcd
Nathan Gardner <38595421+ngardner84@users.noreply.github.com>
2023-07-24 14:37:57 -0700

    Update AddLA4207Printer.sh
    Added directory check

--- AddLA4207Printer.sh
+++ AddLA4207Printer.sh
@@ -10,6 +10,15 @@ printerModel="HP LaserJet Series PCL 4/5"
 # Path to the PPD file in CUPS directory
 ppdFilePath="drv:///sample.drv/laserjet.ppd"
 
+# Path to check
+pharosPath="/Library/Application Support/Pharos"
+
+# Check if Pharos directory exists
+if [ ! -d "$pharosPath" ]; then
+    echo "Directory $pharosPath not found. Please make sure the Pharos software is properly installed."
+    exit 1
+fi
+
 # Add Printer
 lpadmin -p "$printerName" -E -v popup://"$printerAddress"/"$printerName" -m "$ppdFilePath" -D "$printerDescription" -L "$printerLocation"
 
