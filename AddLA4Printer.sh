c66f550c85f139d66542c81db9114c41359110b0
Nathan Gardner <38595421+ngardner84@users.noreply.github.com>
2024-09-04 13:57:31 -0700

    Update AddLA4Printer.sh
    Updated logic for IPP connection instead of SMB share

--- AddLA4Printer.sh
+++ AddLA4Printer.sh
@@ -2,9 +2,8 @@
 
 # Variables
 printerLocation="LA4207"
-printerDescription="Pharos Printer"
+printerDescription="Network Printer"
 printerModel="HP LaserJet Series PCL 4/5"
-printerAddress="10.32.180.109"
 
 # Path to the PPD file in CUPS directory
 ppdFilePath="drv:///sample.drv/laserjet.ppd"
@@ -14,22 +13,27 @@ if [ "$1" == "1" ]; then
     printerName="LA4102Printer"
     printerDescription="LA4 102 Printer"
     printerLocation="LA4 102"
-    
+    printerAddress="10.32.191.198"  # New IP for printer 1
+
 elif [ "$1" == "2" ]; then
     printerName='LA4107Printer'
     printerDescription="LA4 107 Printer"
     printerLocation="LA4 107"
+    printerAddress="10.32.183.23"  # New IP for printer 2
+
 elif [ "$1" == "3" ]; then
     printerName="LA4207Printer"
     printerDescription="LA4 207 Printer"
     printerLocation="LA4 207"
+    printerAddress="10.32.191.246"  # New IP for printer 3
+
 else
     echo "Invalid printer selection. Please use 1, 2, or 3."
     exit 1
 fi
 
 # Add Printer
-lpadmin -p "$printerName" -E -v smb://"$printerAddress"/"$printerName" -m "$ppdFilePath" -D "$printerDescription" -L "$printerLocation"
+lpadmin -p "$printerName" -E -v "ipp://$printerAddress/ipp/print" -m "$ppdFilePath" -D "$printerDescription" -L "$printerLocation"
 
 # Set as Default
 lpoptions -d "$printerName"
